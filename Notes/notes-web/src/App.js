404608aa1bc620262d0d6f0a7b69c6bc23de2d7a
N3WTON <utkarshkumar387@gmail.com>
2020-09-03 20:47:47 -0700

    working on pop-up

--- Notes/notes-web/src/App.js
+++ Notes/notes-web/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.js'
-import Notes from './Card.js'
+import Header from './Header.js';
+import Notes from './Card.js';
+import Edit from './Edit.js';
 
 function App() {
   return (
     <div className="Notes">
       {/* navbar */}
       <Header />
-      <Notes />
+      <div className="notes_Card">
+        <Notes />
+        <Notes />
+        <Notes />
+        <Notes />
+        <Notes />
+        <Notes />
+      </div>
+      <Edit />
       {/* Notes card */}
       {/* add notes button */}
     </div>
